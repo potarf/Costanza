2ce5d674ce9ed6fc23b92cedd9febce93396023b
potarf <jordan_potarf@baylor.edu>
2015-07-15 22:39:27 -0500

    Working Value Changing
    Values can be changed. Objects cannot be renamed

--- JSONeditor.py
+++ JSONeditor.py
@@ -33,30 +33,98 @@ def walk2(inKey, line, searchdb):
             #print tempLine
             searchdb.append(tempLine)
         else:
-            tempLine = tempLine + "~" + str(item)
             #print tempLine
             searchdb.append(tempLine)
             searchdb = walk2(inKey[item], tempLine, searchdb)
     return searchdb
 
-def isCollection(things):
+def descend(dictionary, loc):
+    prev = dictionary
+    newItem = prev[loc.pop(0)]
     try:
-        for thing in things:
-            return True
-    except TypeError:
-        return False
+        descend(prev, dict)
+    except:
+        print newItem
+if len(sys.argv) == 1:
+    print "Usage: \n\t./JSONeditor [File]\n\t./JSONeditor [File] [FileStructurePath]\n\t./JSONeditor [File] [FileStructurePath] [Value]\n\t./JSONeditor [File] [FileStructurePath] [New Name]"
+##Case1 -- print all possible paths
+elif len(sys.argv) == 2:
+    with open(sys.argv[1], 'r') as fp:
+        inputFile = json.load(fp)
+        line = ""
+        searchdb = []
+        searchdb = walk1(inputFile, line, searchdb)
+        for res in searchdb:
+            line = res
+            if line != "None":
+                print line
+##WORK ON BETTER DISPLAY... NESTED??
+##Case 2 -- print values matching search term
+elif len(sys.argv) == 3:
+    with open(sys.argv[1], 'r') as fp:
+        inputFile = json.load(fp)
+        line = ""
+        searchdb = []
+        searchdb = walk1(inputFile, line, searchdb)
+        for res in searchdb:
+            line = str(search(sys.argv[2], res))
+            if line != "None":
+                print line
+##Case 3 -- edit values
+elif len(sys.argv) == 4:
+    with open(sys.argv[1], 'r') as fp:
+        inputFile = json.load(fp)
+        line = ""
+        searchdb = []
+        searchdb = walk1(inputFile, line, searchdb)
+        for res in searchdb:
+            line = str(search(sys.argv[2], res))
+            if line != "None":
+                print line
+                loc = line.split('~')
+                for item in loc:
+                    try:
+                        item = float(item)
+                    except ValueError:
+                        item = item
+                try:
+                    new = float(sys.argv[3])
+                except ValueError:
+                    new = sys.argv[3]
 
+#descend(inputFile[loc.pop(0)], loc)
+                if len(loc) == 2:
+                    inputFile[loc[0]] = new
+                elif len(loc) == 3:
+                    inputFile[loc[0]][loc[1]] = new
+                elif len(loc) == 4:
+                    inputFile[loc[0]][loc[1]][loc[2]] = new
+                elif len(loc) == 5:
+                    inputFile[loc[0]][loc[1]][loc[2]][loc[3]] = new
+                elif len(loc) == 6:
+                    inputFile[loc[0]][loc[1]][loc[2]][loc[3]][loc[4]] = new
+                elif len(loc) == 7:
+                    inputFile[loc[0]][loc[1]][loc[2]][loc[3]][loc[4]][loc[5]] = new
+                elif len(loc) == 8:
+                    inputFile[loc[0]][loc[1]][loc[2]][loc[3]][loc[4]][loc[5]][loc[6]] = new
+                elif len(loc) == 9:
+                    inputFile[loc[0]][loc[1]][loc[2]][loc[3]][loc[4]][loc[5]][loc[6]][loc[7]] = new
+                elif len(loc) == 10:
+                    inputFile[loc[0]][loc[1]][loc[2]][loc[3]][loc[4]][loc[5]][loc[6]][loc[7]][loc[8]] = new
+                print "Change was successful"
 
-with open(sys.argv[1], 'r') as fp:
-    inputFile = json.load(fp)
-    loc = sys.argv[2].split(':')
-    line = ""
-    searchdb = []
-    searchdb = walk1(inputFile, line, searchdb)
-    for res in searchdb:
-        line = str(search(sys.argv[2], res))
-        if line != "None":
-            print line
+##Double check changes
+searchdb = walk1(inputFile, line, searchdb)
+for res in searchdb:
+    line = str(search(sys.argv[2], res))
+    if line != "None":
+        print line
+#                while len(loc) > 0:
+#                    itemloc.pop(0)
+                    #Change name
+                    #check if thelast item passed in argument has a child, or is a child
+#if hasChild(loc, inputFile):
+
 #    for item in loc:
 #        try:
 #            item = float(item)
